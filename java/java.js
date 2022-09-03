7c4a3bdbce0d891ef2bf0f327bab125b87294dac
jtranberg <35013712+jtranberg@users.noreply.github.com>
2022-09-02 20:37:23 -0700

    second commit, added timer and notes

--- java/java.js
+++ java/java.js
@@ -1,12 +1,14 @@
 const cat = new Promise((resolve, reject) => {
+    setTimeout(() => {
     const rand = Math.random();
     if (rand < 0.5) {
         resolve();
     }
     else {
         reject();
-    }
-})
+      }
+    }, 3000);
+});
 cat.then(() => {
     console.log('yahh, we have a cat');
 }) 
